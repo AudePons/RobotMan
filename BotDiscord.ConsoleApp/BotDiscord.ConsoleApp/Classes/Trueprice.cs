ce30a4dea931ee54c020ea48b2fee3e0ca4faf26
Aude PONS <sio.ponsa@gmail.com>
2018-12-19 10:37:51 +0100

    Add files via upload

---
+++ BotDiscord.ConsoleApp/BotDiscord.ConsoleApp/Classes/Trueprice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BotDiscord.ConsoleApp.Classes
+{
+    public sealed class Trueprice
+    {
+        public int price = 0;
+        public int chance;
+        public Random randGen = new Random();
+
+        private static Trueprice instance = null;
+
+        private Trueprice()
+        {
+            price = randGen.Next(0, 1000);
+            chance = 10;
+        }
+        
+        public static Trueprice Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new Trueprice();
+                }
+                return instance;
+            }
+        }
+        
+        public int TheTruePriceValue
+        {
+            get { return price;  }
+        }
+
+        public int NbChance
+        {
+            get { return chance; }
+            set { chance = value; }
+        }
+    }
+}
